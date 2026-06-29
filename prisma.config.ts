c772b472da6524915d164863457ae0b400350341
Julio Cesar Rodrigues Constantino <julio.constantino@ekium.com.br>
2026-06-29 10:01:11 -0300

    correções

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
   datasource: {
-    url: process.env.POSTGRES_URL,
+    url: process.env.POSTGRES_URL_NON_POOLING,
   },
 })
